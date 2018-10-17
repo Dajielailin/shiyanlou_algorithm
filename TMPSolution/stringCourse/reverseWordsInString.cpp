84ab32ebbe30856f261dab72aec0dbe8a211fd3f
dajielailin <dajielailin@126.com>
2018-10-17 18:45:48 +0800

    Reverse words in a string

---
+++ TMPSolution/stringCourse/reverseWordsInString.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string reverseWords(string& inStr)
+{
+	string res;
+	if(!inStr.empty())
+	{
+		string tmp;
+		int start, end;
+		start = end = inStr.size() - 1;
+		for(size_t rindex = end; rindex >= 0; rindex--)
+		{
+			if(inStr[rindex] != ' ')
+			{
+				start--;
+			}
+			esle
+			{
+				if(start != end)
+				{
+					tmp = inStr.substr(start+1, end-start);
+					res += tmp;
+					end = start;
+				}
+				end--, start--;
+			}
+		}
+		if(start != end)
+		{
+			tmp = inStr.substr(start+1, end-start);
+			res += tmp;
+			end = start;
+		}
+		
+	}
+	return res;
+}
+
+int main()
+{
+	string input, output;
+	cin >> input;
+	output = reverseWords(input);
+	cout << output;
+	return 0;
+}
